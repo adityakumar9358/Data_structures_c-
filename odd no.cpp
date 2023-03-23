88750d153fb5a9b05a6982ca7cc40a656341ebc5
adityakumar9358 <127020407+adityakumar9358@users.noreply.github.com>
2023-03-23 10:34:17 +0530

    Create odd no.cpp

---
+++ odd no.cpp
@@ -0,0 +1,22 @@
+#include<iostrems>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    int i = 2;
+
+        if(n%i==0) {
+            cout<< " even no "<<endl;
+        }
+        else{
+            cout<<"prime no"<<endl;
+        }
+        
+
+    }
+
+
+    
