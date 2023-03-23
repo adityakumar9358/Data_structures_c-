0d360bec6936c519da535775452d53f1912a837e
adityakumar9358 <127020407+adityakumar9358@users.noreply.github.com>
2023-03-23 10:32:55 +0530

    Create if and else .cpp

---
+++ if and else .cpp
@@ -0,0 +1,20 @@
+#include<iostrems>
+using namespace std;
+int main(){
+int a ;
+cout<<"enter the value of a"<<endl;
+cin>>a;
+if (a>0)
+{
+cout<<"A IS positive no"<<endl;
+
+}
+else if (a<0)
+{
+cout<<"A is negative no"<<endl;
+
+}
+else{ cout<<"Z is 0"<<endl;
+
+}
+}
